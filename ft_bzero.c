d85e045b57003461f84e1e542985ee342d0deff4
eesuhn <eason.yihong@gmail.com>
2024-02-27 22:18:12 +0800

    bzero, memcpy, memset

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,23 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yilim <yilim@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/27 19:43:56 by yilim             #+#    #+#             */
-/*   Updated: 2024/02/27 19:43:56 by yilim            ###   ########.fr       */
+/*   Created: 2024/02/27 19:55:13 by yilim             #+#    #+#             */
+/*   Updated: 2024/02/27 19:55:13 by yilim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *s, int c, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
-	size_t	count;
-
-	count = 0;
-	while (count < n)
-		((char *)s)[count++] = (unsigned char)c;
-	return (s);
+	ft_memset(s, 0, n);
 }
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,23 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yilim <yilim@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/27 19:43:56 by yilim             #+#    #+#             */
-/*   Updated: 2024/02/27 19:43:56 by yilim            ###   ########.fr       */
+/*   Created: 2024/02/27 20:00:16 by yilim             #+#    #+#             */
+/*   Updated: 2024/02/27 20:00:16 by yilim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *s, int c, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	count;
 
+	if (dest == NULL && src == NULL)
+		return (NULL);
 	count = 0;
 	while (count < n)
-		((char *)s)[count++] = (unsigned char)c;
-	return (s);
+	{
+		((char *)dest)[count] = ((char *)src)[count];
+		count++;
+	}
+	return (dest);
 }
