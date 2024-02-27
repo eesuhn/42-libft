89956f589a1f0b62d777ae10793e07b2d2c79699
eesuhn <eason.yihong@gmail.com>
2024-02-27 22:44:10 +0800

    memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yilim <yilim@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/27 20:00:16 by yilim             #+#    #+#             */
-/*   Updated: 2024/02/27 20:00:16 by yilim            ###   ########.fr       */
+/*   Created: 2024/02/27 20:11:40 by yilim             #+#    #+#             */
+/*   Updated: 2024/02/27 20:11:40 by yilim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t	count;
-
 	if (dest == NULL && src == NULL)
 		return (NULL);
-	count = 0;
-	while (count++ < n)
-		((char *)dest)[count - 1] = ((char *)src)[count - 1];
+	if (dest > src)
+		while (n-- > 0)
+			((char *)dest)[n] = ((char *)src)[n];
+	else
+		ft_memcpy(dest, src, n);
 	return (dest);
 }
